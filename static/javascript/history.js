5a024f00f827c6780301e15adb5314362b31bac4
Manh <manhnguyenhuu7898@yahoo.com.vn>
2017-06-28 15:53:10 +0700

    Testing

---
+++ static/javascript/history.js
@@ -0,0 +1,38 @@
+$(function () {
+    $('.column').equalHeight();
+});
+
+var application = angular.module("history", []);
+
+application.config(['$interpolateProvider', function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('a{');
+    $interpolateProvider.endSymbol('}a');
+}]);
+application.controller("myCtrl", function ($scope, $http) {
+
+    $http({
+        method: 'GET',
+        url: '/return_history'
+    }).then(function successCallback(response) {
+        $scope.past_posts = response.data;
+    }, function errorCallback(response) {
+        console.log("Error");
+    });
+
+    $http({
+        method: 'GET',
+        url: '/return_admin_info'
+    }).then(function successCallback(response) {
+        $scope.name = response.data[0];
+        $scope.avatar = response.data[1];
+        $scope.post_num = response.data[2];
+        $scope.user_num = response.data[3];
+        $scope.superstatus = response.data[4];
+        $scope.admin_id = response.data[5];
+    }, function errorCallback(response) {
+        console.log("Error");
+    });
+
+});
+
+
